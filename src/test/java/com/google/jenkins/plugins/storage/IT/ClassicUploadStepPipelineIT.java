062cde2a93511ba71d02364699a70a9a30837591
Rachel Yen <rachelyen@google.com>
2019-05-18 11:30:04 -0700

    starting pipeline IT

---
+++ src/test/java/com/google/jenkins/plugins/storage/IT/ClassicUploadStepPipelineIT.java
@@ -0,0 +1,37 @@
+package com.google.jenkins.plugins.storage.IT;
+
+import static org.junit.Assert.assertNotNull;
+
+import hudson.EnvVars;
+import java.util.logging.Logger;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.jvnet.hudson.test.JenkinsRule;
+
+public class ClassicUploadStepPipelineIT {
+  private static final Logger LOGGER =
+      Logger.getLogger(ClassicUploadStepPipelineIT.class.getName());
+  @ClassRule public static JenkinsRule jenkinsRule = new JenkinsRule();
+  private static EnvVars envVars;
+  private static String projectId;
+  private static String credentialsId;
+
+  @BeforeClass
+  public static void init() throws Exception {
+    LOGGER.info("Initializing KubernetesEngineBuilderPipelineIT");
+
+    projectId = System.getenv("GOOGLE_PROJECT_ID");
+    assertNotNull("GOOGLE_PROJECT_ID env var must be set", projectId);
+
+    LOGGER.info("Creating credentials");
+    String serviceAccountKeyJson = System.getenv("GOOGLE_CREDENTIALS");
+    assertNotNull("GOOGLE_CREDENTIALS env var must be set", serviceAccountKeyJson);
+    credentialsId = projectId;
+    //    ServiceAccountConfig sac = new StringJsonServiceAccountConfig(serviceAccountKeyJson);
+    //    Credentials c = (Credentials) new GoogleRobotPrivateKeyCredentials(credentialsId, sac,
+    // null);
+    //    CredentialsStore store =
+    //        new SystemCredentialsProvider.ProviderImpl().getStore(jenkinsRule.jenkins);
+    //    store.addCredentials(Domain.global(), c);
+  }
+}
