9c09e5f161d245ea044bd32b61a36aacf4f6f0dd
Rachel Yen <rachelyen@google.com>
2019-06-03 14:40:27 -0700

    classic upload step pipeline it tests work. need to have cleanup steps though

--- src/test/java/com/google/jenkins/plugins/storage/ClassicUploadStepPipelineIT.java
+++ src/test/java/com/google/jenkins/plugins/storage/ClassicUploadStepPipelineIT.java
@@ -6,12 +6,22 @@ import com.cloudbees.plugins.credentials.Credentials;
 import com.cloudbees.plugins.credentials.CredentialsStore;
 import com.cloudbees.plugins.credentials.SystemCredentialsProvider;
 import com.cloudbees.plugins.credentials.domains.Domain;
+import com.google.common.io.ByteStreams;
 import com.google.jenkins.plugins.credentials.oauth.GoogleRobotPrivateKeyCredentials;
 import com.google.jenkins.plugins.credentials.oauth.ServiceAccountConfig;
 import hudson.EnvVars;
+import hudson.model.Result;
+import hudson.model.Run;
+import hudson.slaves.EnvironmentVariablesNodeProperty;
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.util.logging.Logger;
+import org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition;
+import org.jenkinsci.plugins.workflow.job.WorkflowJob;
+import org.jenkinsci.plugins.workflow.job.WorkflowRun;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
+import org.junit.Test;
 import org.jvnet.hudson.test.JenkinsRule;
 
 public class ClassicUploadStepPipelineIT {
@@ -24,12 +34,16 @@ public class ClassicUploadStepPipelineIT {
 
   @BeforeClass
   public static void init() throws Exception {
-    LOGGER.info("Initializing KubernetesEngineBuilderPipelineIT");
+    LOGGER.info("Initializing ClassicUploadStepPipelineIT");
 
     projectId = System.getenv("GOOGLE_PROJECT_ID");
     assertNotNull("GOOGLE_PROJECT_ID env var must be set", projectId);
+    String bucket = System.getenv("GOOGLE_BUCKET");
+    assertNotNull("GOOGLE_BUCKET env var must be set", bucket);
 
-    LOGGER.info("Creating credentials");
+    String pattern = System.getenv("GOOGLE_PATTERN");
+    assertNotNull("GOOGLE_PATTERN env var must be set", pattern);
+
     String serviceAccountKeyJson = System.getenv("GOOGLE_CREDENTIALS");
     assertNotNull("GOOGLE_CREDENTIALS env var must be set", serviceAccountKeyJson);
     credentialsId = projectId;
@@ -38,5 +52,65 @@ public class ClassicUploadStepPipelineIT {
     CredentialsStore store =
         new SystemCredentialsProvider.ProviderImpl().getStore(jenkinsRule.jenkins);
     store.addCredentials(Domain.global(), c);
+
+    EnvironmentVariablesNodeProperty prop = new EnvironmentVariablesNodeProperty();
+    envVars = prop.getEnvVars();
+    envVars.put("CREDENTIALS_ID", credentialsId);
+    envVars.put("BUCKET", bucket);
+    envVars.put("PATTERN", pattern);
+    jenkinsRule.jenkins.getGlobalNodeProperties().add(prop);
+
+    //		      step([$class: 'ClassicUploadStep', credentialsId: env
+    // .JENKINS_TEST_CRED_ID, bucket: "gs://${JENKINS_TEST_BUCKET}", pattern:
+    // env.BUILD_CONTEXT])
+  }
+
+  // test a working one
+  @Test
+  public void testClassicUploadStepSuccessful() throws Exception {
+    try {
+      WorkflowJob testProject = jenkinsRule.createProject(WorkflowJob.class, "test");
+
+      testProject.setDefinition(
+          new CpsFlowDefinition(
+              loadResource(getClass(), "classicUploadStepPipeline.groovy"), true));
+      WorkflowRun run = testProject.scheduleBuild2(0).waitForStart();
+      assertNotNull(run);
+      jenkinsRule.assertBuildStatus(Result.SUCCESS, jenkinsRule.waitForCompletion(run));
+      dumpLog(LOGGER, run);
+    } catch (Exception e) {
+      throw e;
+    }
+  }
+  // test a malformed one
+
+  // TODO: cleanup method. Basically remove buckets (if necessary) and artifacts
+  private static void cleanUp() {}
+
+  /**
+   * TODO: move this to ITUtil Loads the content of the specified resource.
+   *
+   * @param testClass The test class the resource is being loaded for.
+   * @param name The name of the resource being loaded.
+   * @return The contents of the loaded resource.
+   * @throws java.io.IOException If an error occurred during loading.
+   */
+  static String loadResource(Class testClass, String name) throws IOException {
+    return new String(ByteStreams.toByteArray(testClass.getResourceAsStream(name)));
+  }
+
+  /**
+   * TODO: move to ITUtil Dumps the logs from the specified {@link hudson.model.Run}.
+   *
+   * @param logger The {@link java.util.logging.Logger} to be written to.
+   * @param run The {@link hudson.model.Run} from which the logs will be read.
+   * @throws java.io.IOException If an error occurred while dumping the logs.
+   */
+  static void dumpLog(Logger logger, Run<?, ?> run) throws IOException {
+    BufferedReader reader = new BufferedReader(run.getLogReader());
+    String line = null;
+    while ((line = reader.readLine()) != null) {
+      logger.info(line);
+    }
   }
 }
